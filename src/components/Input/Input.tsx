f9b5d8f5bfe6088f203d1789474011ee5666fc57
Gabriel <hermes_moura@outlook.com>
2026-06-04 16:55:38 -0300

    feat: adicionar arquivo bases-react com conceitos fundamentais de React

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,10 @@
 import styles from "./Input.module.css";
 
 type InputProps = {
-  //type: 'text' | 'number' | 'search'; union type
+  type: 'text' | 'number' | 'search';
   id: string;
   labelText: string;
-} & React.ComponentProps<"input">; //inter
+} & React.ComponentProps<"input">; //intersection type
 
 export function Input({id,type,labelText,...rest}: InputProps) {
 
