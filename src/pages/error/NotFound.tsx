c4c03554f5d5ddc995a287a2a01d02a4e401dc47
Myrella Gomes <myrellagomes832@gmail.com>
2026-06-12 11:32:28 -0300

    feat: update NotFound page layout and styles

--- src/pages/error/NotFound.tsx
+++ src/pages/error/NotFound.tsx
@@ -1,19 +1,15 @@
-import { Home } from "lucide-react";
-import { Button } from "../../components/Button/Button";
-import { GenericTemplate } from "../../templates/GenericTemplate";
 import styles from './NotFound.module.css'
+import { MainTemplate } from "../../templates/MainTemplate";
 
 export function NotFound() {
   return (
-    <GenericTemplate>
+    <MainTemplate>
       <div className={styles.title}>
         Page Not Found
       </div>
       <div className={styles.subtitle}>
         This content no longer exists :(
       </div>
-      <Button style={ { fontSize: '1px'}} color="red" icon={<Home />} title="Home">
-      </Button>
-    </GenericTemplate>
+    </MainTemplate>
   );
 }
